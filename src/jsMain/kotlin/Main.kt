61acde03d17188ccccaf6ea82b8f1d1f1dd85322
Greenn3 <asbor1581@yahoo.com>
2021-12-14 23:04:05 +0100

    fhgkdhjf

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -17,57 +17,89 @@ fun main() {
         mutableStateListOf(null, null, null, null)
     )
 
-    fun RandomN (){
+    fun RandomN() {
         val empties = list.sumOf { row -> row.count { cell -> cell == null } }
         val randI = Random.nextInt(empties)
         var count = 0
-        list.forEach { row -> for (index in 0..3){
+        list.forEach { row ->
+            for (index in 0..3) {
 
-            if(row[index] == null){
-                if(count == randI){
-                    row[index] = 2
+                if (row[index] == null) {
+                    if (count == randI) {
+                        row[index] = 2
+                    }
+                    count++
+
                 }
-                count++
-
             }
-        }  }
+        }
+    }
+    fun MoveLeft (){
+        list.forEach { row ->
+            for (index in 1..3){
+                if (row[index] != null){
+                    for(k in 0 until index){
+                        if(row[k] == null){
+                            row[k] = row[index]
+                            row[index] = null
+                        }
+                    }
+                }
+            }
+        }
     }
     RandomN()
     RandomN()
 
     renderComposable(rootElementId = "root") {
-        Table {
+        Div ({
+            style {
+                width(100.percent)
+                height(100.percent)
+            }
+            onKeyDown {
+                console.log(it.key)
+                when(it.key){
+                    "ArrowLeft" -> console.log("bu")
+                    //MoveLeft()
+                }
+            }
+        }){
+            Table {
 
 
-            for (i in 0..3) {
-                Tr {
-                    for (j in 0..3) Td({ style {
-                        border(1.px, LineStyle.Solid, Color.chocolate)
-                        width(25.px)
-                        height(25.px)
-                        textAlign("center")
-                        property("vertical-align", "center")
-                    } }) {
-                        Text(list[i][j]?.toString() ?: "")
+                for (i in 0..3) {
+                    Tr {
+                        for (j in 0..3) Td({
+                            style {
+                                border(1.px, LineStyle.Solid, Color.chocolate)
+                                width(25.px)
+                                height(25.px)
+                                textAlign("center")
+                                property("vertical-align", "center")
+                            }
+                        }) {
+                            Text(list[i][j]?.toString() ?: "")
+                        }
                     }
                 }
             }
-        }
-        Div({ style { padding(25.px) } }) {
-            Button(attrs = {
-                onClick { count -= 1 }
-            }) {
-                Text("-")
-            }
+            Div({ style { padding(25.px) } }) {
+                Button(attrs = {
+                    onClick { count -= 1 }
+                }) {
+                    Text("-")
+                }
 
-            Span({ style { padding(15.px) } }) {
-                Text("$count")
-            }
+                Span({ style { padding(15.px) } }) {
+                    Text("$count")
+                }
 
-            Button(attrs = {
-                onClick { count += 1 }
-            }) {
-                Text("+")
+                Button(attrs = {
+                    onClick { count += 1 }
+                }) {
+                    Text("+")
+                }
             }
         }
     }
