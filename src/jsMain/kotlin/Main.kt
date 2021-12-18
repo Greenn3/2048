588c552651eedb88d58d99d20cd7e5f1966dc1b0
Greenn3 <asbor1581@yahoo.com>
2021-12-18 21:49:04 +0100

    Coś działa

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -2,15 +2,30 @@ import androidx.compose.runtime.*
 import kotlinx.browser.document
 import org.jetbrains.compose.web.attributes.*
 import org.jetbrains.compose.web.attributes.EventsListenerBuilder.Companion.KEYDOWN
+import org.jetbrains.compose.web.attributes.EventsListenerBuilder.Companion.TOUCHEND
+import org.jetbrains.compose.web.attributes.EventsListenerBuilder.Companion.TOUCHSTART
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
+import org.w3c.dom.TouchEvent
 import org.w3c.dom.events.KeyboardEvent
+import org.w3c.dom.get
+import kotlin.math.abs
 import kotlin.random.Random
 
+external interface Options{
+    var passive: Boolean
+}
+var touchStartX: Int? = null
+var touchStartY: Int? = null
 
 fun main() {
+    val options = (js("{}") as Options).apply {
+        passive = false
+    }
+
     var count: Int by mutableStateOf(0)
+
 
     val list: MutableList<MutableList<Int?>> = mutableStateListOf(
         mutableStateListOf(null, null, null, null),
@@ -116,6 +131,39 @@ fun main() {
 
         // console.log((it as KeyboardEvent).key)
     })
+    document.addEventListener(TOUCHSTART, {
+        val event = it as TouchEvent
+        event.preventDefault()
+        touchStartX = event.touches[0]?.pageX
+        touchStartY = event.touches[0]?.pageY
+    }, options)
+    document.addEventListener(TOUCHEND, {
+
+        val event = it as TouchEvent
+        event.preventDefault()
+       val touchEndX = event.changedTouches[0]?.pageX
+       val touchEndY = event.changedTouches[0]?.pageY
+
+        val difX = touchEndX!! - touchStartX!!
+        val difY = touchEndY!! - touchStartY!!
+        if(abs(difX)<abs(difY)){
+            if(difY>0){
+                MoveDown()
+            }
+            if(difY<0){
+                MoveUp()
+            }
+        }
+        if(abs(difX)>abs(difY)){
+            if(difX>0){
+                MoveRight()
+            }
+            if(difX<0){
+                MoveLeft()
+            }
+        }
+    }, options)
+
 
     renderComposable(rootElementId = "root") {
 
